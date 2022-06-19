5337b874f48625d1f4a611aee043335491df34c0
Alamdarali Musa <75349506+alamdarali@users.noreply.github.com>
2022-06-19 21:51:35 +0530

    Create STRING.py

---
+++ STRING.py
@@ -0,0 +1,14 @@
+thisstr = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+
+print(thisstr)
+print(type(thisstr))
+print(len(thisstr))
+
+#fetch world from str using index
+print(thisstr[0:6])
+
+for x in thisstr:
+  print(x)
