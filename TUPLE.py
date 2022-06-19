37ec42db67b730d365b27c7842debac6c6319ce2
Alamdarali Musa <75349506+alamdarali@users.noreply.github.com>
2022-06-19 21:36:05 +0530

    Create TUPLE.py

---
+++ TUPLE.py
@@ -0,0 +1,17 @@
+thistuple = ["apple", "banana", "cherry", "mango", "orange"]
+
+#diff type of value
+mytuple = [2, "name", 10.20, true, "male"]
+
+print(thistuple)
+print(type(thistuple))
+print(len(thistuple))
+
+#fetch value using index
+print(thistuple(1))
+
+#using range
+print(thistuple[2:5])
+
+#using nagative index
+print(thistuple[:-1])
