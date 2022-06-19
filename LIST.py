c41b003427ce4c5aed8dee57eda625679beef888
Alamdarali Musa <75349506+alamdarali@users.noreply.github.com>
2022-06-19 21:33:07 +0530

    Create LIST.py

---
+++ LIST.py
@@ -0,0 +1,19 @@
+#same type of value
+thislist = ["apple", "banana", "cherry", "mango", "orange"]
+
+#diff type of value
+mylist = [2, "name", 10.20, true, "male"]
+
+print(thislist)
+print(type(thislist))
+print(len(thislist))
+
+#fetch value using index
+print(thislist(1))
+
+#using range
+print(thislist[2:5])
+
+#using nagative index
+print(thislist[:-1])
+
