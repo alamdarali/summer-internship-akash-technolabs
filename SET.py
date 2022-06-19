9c97955f8ece35741c5e7a459f017b852f8a3fb3
Alamdarali Musa <75349506+alamdarali@users.noreply.github.com>
2022-06-19 21:39:38 +0530

    Create SET.py

---
+++ SET.py
@@ -0,0 +1,14 @@
+thisset = {"apple", "banana", "cherry", "mango", "orange"}
+
+#diff type of value
+myset = {2, "name", 10.20, true, "male"}
+
+print(thisset)
+print(type(thisset))
+print(len(thisset))
+
+#set not use index so fetch using loop
+for x in thisset:
+  print(x)
+  
+  
