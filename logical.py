383035b8945505b9d4a98675b56f1d54421cb9ca
Alamdarali Musa <75349506+alamdarali@users.noreply.github.com>
2022-06-21 17:20:56 +0530

    Create logical.py

---
+++ logical.py
@@ -0,0 +1,16 @@
+#logical operater using if..else...statement
+
+x = 10
+y = 20
+z = 30
+
+if(z<=x):
+    print("x is greater")
+elif(y>=z):
+    print("y is greater")
+elif(x==y):
+    print("x is equal to y")
+elif(z!=y):
+    print("z is not equal to y")
+else:
+    pass
