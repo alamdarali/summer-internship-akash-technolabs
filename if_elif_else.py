07f023210f76349643b0a0f49b814a1c0d43bf55
Alamdarali Musa <75349506+alamdarali@users.noreply.github.com>
2022-06-21 18:05:06 +0530

    Create if_elif_else.py

---
+++ if_elif_else.py
@@ -0,0 +1,12 @@
+x = 13
+
+if x > 10:
+  print("Above ten,")
+  if x > 20:
+    print("and also above 20!")
+  elif x > 15:
+    print("and below above 20.")
+  else:
+    print("10 to 15")
+else:
+  print("below ten")
