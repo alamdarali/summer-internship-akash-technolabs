ab2e8e9c3cb2fc96e093ee36974ad7fe7f917003
Alamdarali Musa <75349506+alamdarali@users.noreply.github.com>
2022-06-21 19:40:44 +0530

    Create swap.py

---
+++ swap.py
@@ -0,0 +1,10 @@
+x = int(input("enter a number x: "))
+y = int(input("enter a number y: "))
+
+
+print("before swap x is: ", x, "y is: ",y)
+temp = x
+x= y
+y = temp
+print("after swap x is: ", x, "y is: ",y)
+
