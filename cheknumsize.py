45408ded16bb30358b1868f7709b2ef0152ff7d8
Alamdarali Musa <75349506+alamdarali@users.noreply.github.com>
2022-06-21 19:17:59 +0530

    Create cheknumsize.py

---
+++ cheknumsize.py
@@ -0,0 +1,11 @@
+
+n = float(input("Input a number 1: "))
+p = float(input("Input a number 2: "))
+
+if(n>=p):
+    if(n==p):
+        print("1 or 2 are equale")
+    else:
+        print("1 are greater then 2")
+else:
+    print("1 are smaller than 2")
