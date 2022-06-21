e5342d88229b380762249e5c2ce4f9a3afcfdc26
Alamdarali Musa <75349506+alamdarali@users.noreply.github.com>
2022-06-21 19:11:59 +0530

    Create checknum.py

---
+++ checknum.py
@@ -0,0 +1,8 @@
+n = float(input("Input a number: "))
+if n >= 0:
+  if n == 0:
+      print("It is Zero!")
+  else:
+      print("Number is Positive number.")
+else:
+  print("Number is Negative number.")
