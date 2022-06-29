3638d84d057bbb77fdb463348be27f6d711d5c5d
Alamdarali Musa <75349506+alamdarali@users.noreply.github.com>
2022-06-29 09:43:22 +0530

    Create diffArgument.py

---
+++ diffArgument.py
@@ -0,0 +1,11 @@
+#function define with argument
+def sum(a=5, b=8):
+    print(a+b)
+
+#function call with value
+sum(10, 20)
+#function call without argument
+sum()
+#function call with key & value
+sum(b=15, a=17)
+
